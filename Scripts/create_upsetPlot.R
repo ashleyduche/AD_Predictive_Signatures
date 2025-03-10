41caaf23720dacbeac24159f06d869860f976a72
ashleyduche <71372500+ashleyduche@users.noreply.github.com>
2025-03-10 12:45:58 -0700

    Update create_upsetPlot.R

--- Scripts/create_upsetPlot.R
+++ Scripts/create_upsetPlot.R
@@ -3,10 +3,10 @@ if(!requireNamespace("UpSetR", quietly=TRUE)) install.packages("UpSetR")
 library(UpSetR)
 
 #Source create_binaryMtx function to prepare data format
-source("~/Data_Processing/AD_signature_predictions/create_binaryMtx.R")
+source("~/AD_signature_predictions/Scripts/create_binaryMtx.R")
 
 #Upload example data 
-df<-read.csv("~/AD_signature_predictions/data/gene_region_data.csv", stringsAsFactors=FALSE)
+df<-read.csv("~/AD_signature_predictions/Data/gene_region_data.csv", stringsAsFactors=FALSE)
 
 #Run function on example data specifying the number of regions genes are shared across 
 common_genes_4<-create_binaryMtx(df, min_regions=4)
